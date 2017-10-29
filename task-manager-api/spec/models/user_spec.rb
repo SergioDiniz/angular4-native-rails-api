22a1c2ea21463f031f14e31ba73dcbf856626a23
Sergio Diniz <sergiodinizsh@gmail.com>
2017-10-29 06:56:26 -0300

    testando metodos com mock

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { build(:user) }
+
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'retun email end created_at' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return("asd123zxcTOKEN")
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: asd123zxcTOKEN")
+    end
+  end
 end
