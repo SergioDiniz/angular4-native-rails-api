aed389b668b7e0c7983a50977f9db06d3cb29430
Sergio Diniz <sergiodinizsh@gmail.com>
2017-10-29 21:17:57 -0300

    gerador de token basico

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe User, type: :model do
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: asd123zxcTOKEN")
     end
   end
+
+  describe '#generate_authetication_token!' do
+    it 'generetes a unique ayth token' do
+      allow(Devise).to receive(:friendly_token).and_return("asd123zxcTOKEN")
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq("asd123zxcTOKEN")
+    end
+
+    it 'generetes another auth token when the current token is already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return('asd123tokenxyz', 'asd123tokenxyz', 'abc321token2')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
 end
