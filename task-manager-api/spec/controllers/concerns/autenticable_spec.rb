47df92aaae1281df4d7a88f79db003e10b4f9bab
Sergio Diniz <sergiodinizsh@gmail.com>
2017-11-01 09:48:55 -0300

    iniciando restrições de ações, e testes

--- task-manager-api/spec/controllers/concerns/autenticable_spec.rb
+++ task-manager-api/spec/controllers/concerns/autenticable_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Authenticable do
 
   let(:app_controller) {subject}
 
-  describe '#corrent_user' do
+  describe '#current_user' do
     let(:user) {create(:user)}
 
     before do
@@ -17,6 +17,30 @@ RSpec.describe Authenticable do
 
     it 'return the user from the authorization header' do
       expect(app_controller.current_user).to eq(user)
+    end
+  end
+
+  describe '#authentication_with_token!' do
+    controller do
+      before_action :authentication_with_token!
+
+      def restricted_action; end
+    end
+
+    context 'when there is no user logged in' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+        routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
+        get :restricted_action
+      end
+
+      it 'when status code 401' do
+        expect(response).to have_http_status(401)
+      end
+
+      it 'return json data for the errors' do
+        expect(json_body).to have_key(:errors)
+      end
     end
   end
 end
